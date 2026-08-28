d55a1b343eeb76de3c0231e1d5bb3cc82df6120c
Janner121 <valajanner@gmail.com>
2026-08-27 20:50:01 -0500

    feature: mejorar experiencia visual del inicio de sesión

--- resources/js/components/input-error.tsx
+++ resources/js/components/input-error.tsx
@@ -3,16 +3,21 @@ import { cn } from '@/lib/utils';
 export default function InputError({
     message,
     className,
+    id,
 }: {
     message?: string;
     className?: string;
+    id?: string;
 }) {
     if (!message) {
         return null;
     }
 
     return (
-        <p className={cn('text-sm font-semibold text-destructive', className)}>
+        <p
+            id={id}
+            className={cn('text-sm font-semibold text-destructive', className)}
+        >
             {message}
         </p>
     );
